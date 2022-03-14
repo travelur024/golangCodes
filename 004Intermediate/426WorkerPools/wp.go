39986f77b7af4eda41b61bca26e03e4ff908c925
Jonathan Espinosa <jon_esp_esq@hotmail.com>
2022-03-14 13:26:20 -0600

    Finished class 27 Multiplexacion

---
+++ 004Intermediate/426WorkerPools/wp.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	tasks := []int{2, 3, 4, 5, 7, 10, 12, 40}
+	nWorkers := 1
+	jobs := make(chan int, len(tasks))
+	result := make(chan int, len(tasks))
+
+	//for i := 0; i < nWorkers; i++ {
+	go Worker(0, jobs, result)
+	go Worker(nWorkers, jobs, result)
+	//}
+
+	for _, value := range tasks {
+		jobs <- value
+	}
+	close(jobs)
+
+	for r := 0; r < len(tasks); r++ {
+		<-result
+	}
+}
+
+func Worker(id int, jobs <-chan int, result chan<- int) {
+	for job := range jobs {
+		fmt.Printf("Worker with id %d started fib with %d\n", id, job)
+		fib := Fibonacci(job)
+		fmt.Printf("Worked with id %d, job %d and fib %d\n", id, job, fib)
+		result <- fib
+	}
+}
+
+func Fibonacci(n int) int {
+	if n <= 1 {
+		return n
+	}
+	return Fibonacci(n-1) + Fibonacci(n-2)
+}
