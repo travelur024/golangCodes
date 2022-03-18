6063042f06f0a12a17a9e1e24b28374ff070eaf0
Jonathan Espinosa <jon_esp_esq@hotmail.com>
2022-03-18 11:01:13 -0600

    Finish advanced class 3

--- 005Advanced/503Mutex/main.go
+++ 005Advanced/503Mutex/main.go
@@ -9,10 +9,13 @@ var (
 	balance int = 100
 )
 
-func Deposit(amount int, wg *sync.WaitGroup) {
+//Llamando a lock en la funcion que bloquea la variable a modificar
+func Deposit(amount int, wg *sync.WaitGroup, lock *sync.Mutex) {
 	defer wg.Done()
+	lock.Lock()
 	b := balance
 	balance = b + amount
+	lock.Unlock()
 }
 
 func Balance() int {
@@ -22,9 +25,11 @@ func Balance() int {
 
 func main() {
 	var wg sync.WaitGroup
-	for i := 1; i <= 5; i++ {
+	//Lock
+	var lock sync.Mutex
+	for i := 1; i <= 5000; i++ {
 		wg.Add(1)
-		go Deposit(i*100, &wg)
+		go Deposit(i*100, &wg, &lock)
 	}
 	wg.Wait()
 	fmt.Println(balance)
